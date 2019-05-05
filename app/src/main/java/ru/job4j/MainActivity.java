7178e6b5e03f67f3ee6e2ff0148fb2f77725c2fc
MaximShavva <saloedos@yandex.ru>
2019-05-06 00:04:24 +0300

    1. Activity. Жизненный цикл.[#140221]
    1.  Добавьте onSaveInstanceState в код.
    2.  Создайте переменную в классе Activity для счетчика переворотов устройства.
    3.  Добавьте вывод значения переменной в лог для проверки сохранения значения.
    4.  Переопределите метод onSaveInstanceState так, чтобы значение счетчика сохранялось после переворота.
    5.  Используйте метод onCreate для восстановления сохраненных данных.

--- app/src/main/java/ru/job4j/MainActivity.java
+++ app/src/main/java/ru/job4j/MainActivity.java
@@ -2,12 +2,78 @@ package ru.job4j;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+/**
+ * Класс для проверки методов жизненного цикла Activity.
+ * Содержит счётчик поворотов экрана и вывод его в ЛОГ.
+ *
+ * @author Шавва Максим.
+ * @version 1.
+ * @since 5.05.2019г.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Используется для фильтрации вывода в LogCat.
+     */
+    private static final String TAG = "ExamActivity";
+
+    /**
+     * Содержит счётчик числа поворотов экрана.
+     */
+    private int spins;
+
+    /**
+     * @param savedInstanceState Используем для восстановления значения spins.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        if (savedInstanceState != null) {
+            this.spins = savedInstanceState.getInt("spins", -1);
+            Log.d(TAG, String.format("Количество поворотов экрана = %d", spins));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    /**
+     * @param outState Увеличиваем переменную spins и сохраняем её в bundle.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("spins", ++spins);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
