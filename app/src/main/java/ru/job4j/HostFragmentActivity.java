97a9a0c13864efb6e68ff5d2775e14b70c0e9c9e
MaximShavva <saloedos@yandex.ru>
2019-05-08 18:27:32 +0300

    4. UI-Фрагменты.[#140225]

---
+++ app/src/main/java/ru/job4j/HostFragmentActivity.java
@@ -0,0 +1,68 @@
+package ru.job4j;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Класс - Активность - тест использования фрагментов в активности.
+ *
+ * @author Шавва Максим.
+ * @version 1.
+ * @since 8.05.2019г.
+ */
+public class HostFragmentActivity
+        extends AppCompatActivity
+        implements FirstFragment.OnNextButtonClickListener,
+        SecondFragment.OnBackButtonClickListener {
+
+    private FragmentManager fm;
+    private Fragment firstFragment;
+    private Fragment secondFragment;
+
+    /**
+     * Добавляем первый фрагмент в активность.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_fragment);
+        fm = getSupportFragmentManager();
+        firstFragment = fm.findFragmentById(R.id.fragment_container);
+        if (firstFragment == null) {
+            firstFragment = new FirstFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, firstFragment)
+                    .commit();
+        }
+    }
+
+    /**
+     * Меняем первый фрагмент на второй.
+     */
+    @Override
+    public void onNextButtonClicked(String message) {
+        Bundle bundle = new Bundle();
+        bundle.putString("message", message);
+        if (secondFragment == null) {
+            secondFragment = new SecondFragment();
+        }
+        secondFragment.setArguments(bundle);
+        fm.beginTransaction()
+                .replace(R.id.fragment_container, secondFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    /**
+     * Меняем второй фрагмент на первый.
+     */
+    @Override
+    public void onBackButtonClicked(String message) {
+        fm.popBackStackImmediate();
+        TextView text = findViewById(R.id.message_back);
+        text.setText(message);
+    }
+}
---
+++ app/src/main/java/ru/job4j/SecondFragment.java
@@ -0,0 +1,76 @@
+package ru.job4j;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Класс - второй фрагмент.
+ *
+ * @author Шавва Максим.
+ * @version 1.
+ * @since 8.05.2019г.
+ */
+public class SecondFragment extends Fragment {
+
+    /**
+     * Ссылка на кнопку во фрагменте.
+     */
+    private Button previous;
+
+    private OnBackButtonClickListener callback;
+
+    public interface OnBackButtonClickListener {
+        void onBackButtonClicked(String message);
+    }
+
+    public void onClick(View view) {
+        callback.onBackButtonClicked("Back button clicked");
+    }
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Назначаем активность при присоединении фрагмента к активити.
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callback = (OnBackButtonClickListener) context;
+    }
+
+    /**
+     * Наполняем фрагмент представлениями, присваиваем слушателя кнопке,
+     * Извлекаем переданный текст в TextView message.
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        previous = view.findViewById(R.id.back);
+        previous.setOnClickListener(this::onClick);
+        TextView message = view.findViewById(R.id.message);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            message.setText(bundle.getString("message", "default"));
+        }
+        return view;
+    }
+
+    /**
+     * Обнуляем ссылку при отсоединении фрагмента от активити.
+     */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+}
